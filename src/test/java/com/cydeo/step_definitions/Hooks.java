16e065b08662084f60a06934310bc760bfff81c1
orhunpusat <orhunpusat3000@gmail.com>
2022-06-16 01:03:14 +0300

    day 15 hooks java

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -5,7 +5,9 @@ package com.cydeo.step_definitions;
     */
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 public class Hooks {
     //import from io.cucumber.java not from junit
@@ -16,6 +18,15 @@ public class Hooks {
     @After
     public void teardownScenario(){
         System.out.println("====Closing browser using cucumber @After");
+        System.out.println("====Scenario ended/ Take screenshot if failed");
+    }
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("-------applying setup using @BeforeStep");
+    }
+    @AfterStep
+    public void afterStep(){
+        System.out.println("------applying tearDown using @AfterStep");
     }
 
 
