b0ef2bdb0956ca7653f3e97a48e19df979189fed
orhunpusat <orhunpusat3000@gmail.com>
2022-06-16 15:39:43 +0300

    day 16

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -13,11 +13,19 @@ public class GoogleStepDefinitions {
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
     @When("user types {string} and clicks enter")
-    public void user_types_and_clicks_enter(String string) {
+    public void user_types_and_clicks_enter(String searchKeyword) {
 
+        googleSearchPage.searchBox.sendKeys(searchKeyword+Keys.ENTER);
     }
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
+
+        String expectedTitle = string+" - Google'da Ara";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertions accepts first arg as expected, second arg as actual
+        Assert.assertEquals( "Title is not as expected",expectedTitle, actualTitle);
+
 
     }
 
