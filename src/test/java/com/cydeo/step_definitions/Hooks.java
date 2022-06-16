fb5378d27ff405885bf6853b3e5333434a5e8f2a
orhunpusat <orhunpusat3000@gmail.com>
2022-06-17 00:14:08 +0300

    day 16 hooks take screen shot

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -5,10 +5,7 @@ package com.cydeo.step_definitions;
     */
 
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -30,9 +27,10 @@ public class Hooks {
     }
 
     @After
-    public void teardownScenario(){
+    public void teardownScenario(Scenario scenario){
 
         byte [] screenShot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenShot,"image/png",scenario.getName());
 
         Driver.closeDriver();
 
