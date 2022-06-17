9a5b463f97f1c6180f54465f377044878162e14e
orhunpusat <orhunpusat3000@gmail.com>
2022-06-17 10:15:10 +0300

    day 16 wiki search feature

--- src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
@@ -34,4 +34,9 @@ public class Wiki_StepDefinitions {
         //verify actual title contains string (coming from feature file )
         Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
     }
+
+    @Then("User sees Steve Jobs is in the main header")
+    public void userSeesSteveJobsIsInTheMainHeader() {
+
+    }
 }
