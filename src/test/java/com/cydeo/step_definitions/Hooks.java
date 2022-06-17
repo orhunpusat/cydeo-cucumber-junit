344ccfcc37ee0a628fed9e47964d56b26edbdecc
orhunpusat <orhunpusat3000@gmail.com>
2022-06-17 09:13:22 +0300

    day 16 hooks take screen shot

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -29,8 +29,14 @@ public class Hooks {
     @After
     public void teardownScenario(Scenario scenario){
 
-        byte [] screenShot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-        scenario.attach(screenShot,"image/png",scenario.getName());
+        //scenario.isFailed() -->if scenario fails this method will return TRUE boolean value
+
+        if (scenario.isFailed()){
+            byte [] screenShot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenShot,"image/png",scenario.getName());
+        }
+
+
 
         Driver.closeDriver();
 
