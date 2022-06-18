f2fe9c2c57b34224fc70522e649c2eb6c4d2db2b
orhunpusat <orhunpusat3000@gmail.com>
2022-06-18 14:16:50 +0300

    utilities

--- src/test/java/com/cydeo/pages/WikiSearchPage.java
+++ src/test/java/com/cydeo/pages/WikiSearchPage.java
@@ -8,11 +8,11 @@ import org.openqa.selenium.support.PageFactory;
 public class WikiSearchPage {
 
     public WikiSearchPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(id = "searchInput")
-    public WebElement serchBox;
+    public WebElement searchBox;
 
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement searchButton;
--- src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
@@ -13,26 +13,27 @@ public class Wiki_StepDefinitions {
 
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
-
-        Driver.getDriver().get("https://www.wikipedia.org/");
+        Driver.getDriver().get("https://www.wikipedia.org");
     }
 
     @When("User types {string} in the wiki search box")
     public void user_types_in_the_wiki_search_box(String string) {
 
-        wikiSearchPage.serchBox.sendKeys(string);
+        wikiSearchPage.searchBox.sendKeys(string);
+
     }
 
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
 
         wikiSearchPage.searchButton.click();
-    }
 
+    }
     @Then("User sees {string} is in the wiki title")
     public void user_sees_is_in_the_wiki_title(String string) {
-        //verify actual title contains string (coming from feature file )
+        //verify actual title contains string (coming from feature file)
         Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
+
     }
 
     @Then("User sees {string} is in the main header")
@@ -41,5 +42,6 @@ public class Wiki_StepDefinitions {
         Assert.assertTrue(wikiSearchPage.mainHeader.isDisplayed());
 
         Assert.assertTrue(wikiSearchPage.mainHeader.getText().equals(string));
+
     }
 }
