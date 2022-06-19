f2d55ed47612fe3142d06c46dc792f451a9ef92a
orhunpusat <orhunpusat3000@gmail.com>
2022-06-19 18:08:16 +0300

    day17 dropdown data table

--- src/test/java/com/cydeo/pages/DropdownsPage.java
+++ src/test/java/com/cydeo/pages/DropdownsPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DropdownsPage {
@@ -8,4 +10,6 @@ public class DropdownsPage {
     public DropdownsPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(id="month")
+    public WebElement monthDropdown;
 }
--- src/test/java/com/cydeo/step_definitions/DataTable_StepDefinition.java
+++ src/test/java/com/cydeo/step_definitions/DataTable_StepDefinition.java
@@ -1,9 +1,14 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.DropdownsPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTable_StepDefinition {
@@ -19,8 +24,26 @@ public class DataTable_StepDefinition {
     public void user_is_on_the_dropdowns_page_of_practice_tool() {
         Driver.getDriver().get("https://practice.cydeo.com/dropdown");
     }
+
+    
+    DropdownsPage dropdownsPage = new DropdownsPage();
+
     @Then("User should see below info in month dropdown")
     public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonths) {
         System.out.println("expectedMonths = " + expectedMonths);
+
+        Select select = new Select(dropdownsPage.monthDropdown);
+        List<WebElement> actualOptionsAsWebElement = select.getOptions();
+
+        //Create a list of string and pass all of the actual web element options string into that new list
+
+        List<String> actualOptionsAsString = new ArrayList<>();
+
+        for (WebElement each : actualOptionsAsWebElement) {
+            actualOptionsAsString.add(each.getText());
+        }
+
+        Assert.assertEquals(expectedMonths,actualOptionsAsString);
+        //Assert will check the size of the lists first. If it is matching it will check content 1 by 1.
     }
 }
