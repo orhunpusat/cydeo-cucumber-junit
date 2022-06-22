6eee20de4e0fc6a5374f9e84e3c94d634973f756
orhunpusat <orhunpusat3000@gmail.com>
2022-06-22 22:13:28 +0300

    day18 Driver.java with ThreadLocal

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -20,14 +20,16 @@ public class Driver {
     We make WebDriver private, because we want to close access from outside the class.
     We make it static because we will use it in a static method
      */
-    private static WebDriver driver;
+    //private static WebDriver driver;
+
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
 
     /*
     Create a reusable utility method which will return same driver instance when we call it
      */
     public static WebDriver getDriver() {
-        if (driver == null) {
+        if (driverPool.get() == null) {
             //We read our browserType from configuration.properties.
             //This way we can control which browser is opened from outside our code from configuration.properties.
             String browserType = ConfigurationReader.getProperty("browser");
@@ -37,29 +39,29 @@ public class Driver {
             switch (browserType) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
             }
 
         }
-        return driver;
+        return driverPool.get();
     }
 
     /*
     This method will make sure our driver value is always null after using quit() method
      */
     public static void closeDriver() {
-        if (driver != null) {
-            driver.quit();// this line will terminate the existing session. value will not even be null
-            driver = null;
+        if (driverPool.get() != null) {
+            driverPool.get().quit();// this line will terminate the existing session. value will not even be null
+            driverPool.remove();
         }
     }
 }
